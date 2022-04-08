99b9e23feb035d7d3611840504b77bc083d23775
retxrika <47078259+retxrika@users.noreply.github.com>
2022-04-08 17:10:44 +0500

    Добавил второй проект, решил первую задачу

--- AngryBirds/AngryBirdsTask.cs
+++ AngryBirds/AngryBirdsTask.cs
@@ -12,7 +12,7 @@ namespace AngryBirds
 		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
 		public static double FindSightAngle(double v, double distance)
 		{
-			return Math.PI / 4;
+			return 0.5 * Math.Asin(distance * 9.8 / Math.Pow(v, 2));
 		}
 	}
 }
